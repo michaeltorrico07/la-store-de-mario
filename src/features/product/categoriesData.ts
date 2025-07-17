de30084e5490f697e5b2f3ab3b155e4a329cb13b
michaeltorrico07 <michaeltorrico1804@gmail.com>
2025-07-17 10:51:02 -0300

    ma el gordo hid se comio mi tarea y me puso 0

--- src/features/product/categoriesData.ts
+++ src/features/product/categoriesData.ts
@@ -6,7 +6,6 @@ export const categoriesData = (products: Product[] | null) => {
     { id: 'Todos', label: 'Todos', count: products.length },
     { id: 'Comida', label: 'Comidas', count: products.filter(p => p.category === 'Comida').length },
     { id: 'Bebida', label: 'Bebidas', count: products.filter(p => p.category === 'Bebida').length },
-    { id: 'Postre', label: 'Postres', count: products.filter(p => p.category === 'Snack').length },
-    { id: 'Snack', label: 'Snacks', count: products.filter(p => p.category === 'Carpincho').length }
+    { id: 'Postre', label: 'Postres', count: products.filter(p => p.category === 'Snack').length }
   ]
 } 
