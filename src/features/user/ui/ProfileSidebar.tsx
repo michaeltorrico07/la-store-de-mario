0faeb22a738047baa51173df26d6487944f0e882
michaeltorrico07 <michaeltorrico1804@gmail.com>
2025-08-16 16:31:58 -0300

    ee ee e eee e ee

--- src/features/user/ui/ProfileSidebar.tsx
+++ src/features/user/ui/ProfileSidebar.tsx
@@ -37,43 +37,43 @@ export const ProfileSidebar = ({ user, activeTab, handleTabChange, handleCall }:
 
         <button
           onClick={() => handleTabChange('datos')}
-          className={`cursor-pointer w-full py-3 px-4 rounded-lg transition-all duration-300 ease-in-out flex items-center transform hover:scale-105 active:scale-95 ${activeTab === 'datos'
-            ? 'bg-red-600 text-white shadow-lg shadow-red-600/30 translate-x-1'
-            : 'bg-gray-700 hover:bg-gray-600 text-white hover:translate-x-1'
+          className={`cursor-pointer w-full py-3 px-4 rounded-lg transition-all duration-200 ease-in-out flex items-center transform hover:scale-105 active:scale-95 ${activeTab === 'datos'
+            ? 'bg-gray-700 hover:bg-gray-700 text-white shadow-lg shadow-red-600/30 translate-x-1'
+            : 'bg-red-600 hover:bg-gray-700 text-white hover:translate-x-1'
             }`}
         >
-          <User className={`w-5 h-5 mr-3 transition-all duration-300 ${activeTab === 'datos' ? 'text-white' : 'text-gray-300'
+          <User className={`w-5 h-5 mr-3 transition-all duration-200 ${activeTab === 'datos' ? 'text-white' : 'text-gray-300'
             }`} />
-          <span className="transition-all duration-300">Datos personales</span>
+          <span className="transition-all duration-200">Datos personales</span>
         </button>
 
         <button
           onClick={() => { handleTabChange('historial'); handleCall() }}
-          className={`cursor-pointer w-full py-3 px-4 rounded-lg transition-all duration-300 ease-in-out flex items-center transform hover:scale-105 active:scale-95 ${activeTab === 'historial'
-            ? 'bg-red-600 text-white shadow-lg shadow-red-600/30 translate-x-1'
-            : 'bg-gray-700 hover:bg-gray-600 text-white hover:translate-x-1'
+          className={`cursor-pointer w-full py-3 px-4 rounded-lg transition-all duration-200 ease-in-out flex items-center transform hover:scale-105 active:scale-95 ${activeTab === 'historial'
+            ? 'bg-gray-700 hover:bg-gray-700 text-white shadow-lg shadow-red-600/30 translate-x-1'
+            : 'bg-red-600 hover:bg-gray-700 text-white hover:translate-x-1'
             }`}
         >
-          <Clock className={`w-5 h-5 mr-3 transition-all duration-300 ${activeTab === 'historial' ? 'text-white' : 'text-gray-300'
+          <Clock className={`w-5 h-5 mr-3 transition-all duration-200 ${activeTab === 'historial' ? 'text-white' : 'text-gray-300'
             }`} />
-          <span className="transition-all duration-300">Historial de pedidos</span>
+          <span className="transition-all duration-200">Historial de pedidos</span>
         </button>
 
         {user.rol === 'ADMIN' && (
           <Link
             to={'/management'}
-            className={`cursor-pointer bg-gray-700 w-full py-3 px-4 rounded-lg transition-all duration-300 ease-in-out flex items-center transform hover:scale-105 active:scale-95 hover:bg-gray-600 text-white`}
+            className={`cursor-pointer bg-red-600 w-full py-3 px-4 rounded-lg transition-all duration-200 ease-in-out flex items-center transform hover:scale-105 active:scale-95 hover:bg-gray-700 text-white`}
           >
-            <Cog className={`w-5 h-5 mr-3 transition-all duration-300`} />
-            <span className="transition-all duration-300">Página de gestión</span>
+            <Cog className={`w-5 h-5 mr-3 transition-all duration-200`} />
+            <span className="transition-all duration-200">Página de gestión</span>
           </Link>
         )}
       </div>
 
       {/* Botón cerrar sesión */}
-      <button onClick={() => {SignOut(); dispatch(resetListProducts())}} className="cursor-pointer w-full bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded-lg transition-all duration-300 flex items-center justify-center hover:scale-105 active:scale-95 hover:shadow-lg hover:shadow-red-700/30">
+      <button onClick={() => {SignOut(); dispatch(resetListProducts())}} className="cursor-pointer w-full bg-red-600 hover:bg-gray-700 text-white py-2 px-4 rounded-lg transition-all duration-300 flex items-center justify-center hover:scale-105 active:scale-95 hover:shadow-lg hover:shadow-red-700/30">
         <svg
-          className="w-5 h-5 mr-2 transition-transform duration-300 hover:rotate-12"
+          className="w-5 h-5 mr-2 transition-transform duration-200 hover:rotate-12"
           fill="none"
           stroke="currentColor"
           viewBox="0 0 24 24"
@@ -85,7 +85,7 @@ export const ProfileSidebar = ({ user, activeTab, handleTabChange, handleCall }:
             d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013 3v1"
           />
         </svg>
-        <span className="transition-all duration-300">Cerrar sesión</span>
+        <span className="transition-all duration-200">Cerrar sesión</span>
       </button>
     </div>
   );
