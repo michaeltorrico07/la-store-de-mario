18e1c54c4878692e5387f565175761474509511a
michaeltorrico07 <michaeltorrico1804@gmail.com>
2025-08-16 01:39:43 -0300

    comete este sanguchoto

--- src/features/product/ui/carousel.tsx
+++ src/features/product/ui/carousel.tsx
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css'
+import 'swiper/swiper-bundle.css';
 import type { Product } from '../product';
 import { Link } from 'react-router-dom';
 
