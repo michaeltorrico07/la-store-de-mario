389b3f1aae62efbc5ffb28e3dd7382e657c665cc
michaeltorrico07 <michaeltorrico1804@gmail.com>
2025-08-21 11:15:56 -0300

    gian gei

--- src/features/product/categoriesData.ts
+++ src/features/product/categoriesData.ts
@@ -5,6 +5,6 @@ export const categoriesData = (products: Product[] ) => {
     { id: 'Todos', label: 'Todos', count: products.length },
     { id: 'Comida', label: 'Comidas', count: products.filter(p => p.category === 'Comida').length },
     { id: 'Bebida', label: 'Bebidas', count: products.filter(p => p.category === 'Bebida').length },
-    { id: 'Postre', label: 'Postres', count: products.filter(p => p.category === 'Snack').length }
+    { id: 'Snack', label: 'Snacks', count: products.filter(p => p.category === 'Snack').length }
   ]
 } 
